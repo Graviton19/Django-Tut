092845ffefa7d090f072022a8dcff8dffb10c360
Graviton19 <aalekhbukhariya@gmail.com>
2024-06-21 11:37:23 +0530

    models and migrations

--- Djangotutorial/Django/urls.py
+++ Djangotutorial/Django/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_django,name = 'django'),
+    path('<int:chai_id>/',views.chai_detail,name= 'chai_description')
 ]
--- Djangotutorial/Django/views.py
+++ Djangotutorial/Django/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .models import ChaiVarity
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def all_django(request):
-    return render(request,'Django/all_django.html')
+    chais = ChaiVarity.objects.all()
+    return render(request,'Django/all_django.html',{'chais':chais})
+
+def chai_detail(request, chai_id):
+    chai = get_object_or_404(ChaiVarity, pk= chai_id)
+    return render(request,'Django/chai_detail.html',{'chai':chai})
